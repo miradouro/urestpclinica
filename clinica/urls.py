d02bc9c2a04e6a2a2dedb75c859771d550871a9a
Miradouro <rafaelmiradouro@gmail.com>
2021-04-08 14:59:29 -0300

    adicionado app imagens

--- clinica/urls.py
+++ clinica/urls.py
@@ -1,16 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework import routers
 from pacientes.api.viewsets import PacientesViewset
 from agendamentos.api.viewsets import AgendamentosViewSet
 from historicos.api.viewsets import HistoricosViewSet
+from imagens.api.viewsets import ImagesHistoricoViewSet
 
 route = routers.DefaultRouter()
-route.register(r'pacientes', PacientesViewset, basename='pacientes')
-route.register(r'agendamentos', AgendamentosViewSet, basename='agendamentos')
-route.register(r'historicos', HistoricosViewSet, basename='historicos')
+route.register(r'pacientes', PacientesViewset)
+route.register(r'agendamentos', AgendamentosViewSet)
+route.register(r'historicos', HistoricosViewSet)
+route.register(r'imagens_historicos', ImagesHistoricoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(route.urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
